05eb4c4e905d4a5bc68e0695b6490a3bd4ad4f21
Freer <soulshined@users.noreply.github.com>
2024-07-31 15:02:37 -0500

    Add Join-PSScriptRootPath alias func

--- powershell/Modules/Freer.Utility/Freer.Utility.psm1
+++ powershell/Modules/Freer.Utility/Freer.Utility.psm1
@@ -1,1 +1,19 @@
-# INTENTIONALLY LEFT BLANK
+function Join-PSScriptRootPath {
+    [Alias("jpsp")]
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0)]
+        [string]$ChildPath,
+        [Parameter(ValueFromRemainingArguments = $true, ValueFromPipeline = $true, Position = 1)]
+        [string[]]$AdditionalChildPath,
+
+        [switch]$Resolve
+    )
+
+    process {
+        Join-Path -Path $MyInvocation.PSScriptRoot `
+            -ChildPath $ChildPath `
+            -AdditionalChildPath $AdditionalChildPath `
+            -Resolve:$Resolve
+    }
+}
