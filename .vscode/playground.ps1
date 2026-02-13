c0519237650234c4e74fdbe126f1ad6670c276aa
Freer <soulshined@users.noreply.github.com>
2026-02-13 16:50:26 -0600

    Remove vscode prescence

--- .vscode/playground.ps1
+++
@@ -1,6 +0,0 @@
-$Env:SCAFFOLD_HOME = Join-Path $PSScriptRoot scaffolds
-
-$ErrorActionPreference = 'Stop'
-$VerbosePreference = 'Continue'
-
-$Env:PSModulePath += [IO.Path]::PathSeparator + (Join-Path $PSScriptRoot '..' 'powershell' 'Modules' -Resolve)
